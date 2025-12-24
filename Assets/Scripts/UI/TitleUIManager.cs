f5472d58635b63e2bd38aed76ed25e4732d595ad
51hamu51 <hawks_2023@outlook.jp>
2025-12-24 22:51:19 +0900

    遊び方パネルのアニメーション

--- Assets/Scripts/UI/TitleUIManager.cs
+++ Assets/Scripts/UI/TitleUIManager.cs
@@ -1,31 +1,67 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class TitleUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject howToPlayPanel;
-
     [SerializeField] private GameObject asobikata;
     [SerializeField] private GameObject modeExplain;
 
-    [SerializeField] private GameObject menuButtons;
+    [Header("パネルのアニメーション用")]
+    [SerializeField] private float animDuration;
+    [SerializeField] private float moveY;
+    [SerializeField] private RectTransform panelRect;
+    [SerializeField] private CanvasGroup canvasGroup;
+    private Vector2 defaultPos;
+
 
     void Start()
     {
-        PanelClose();
+        defaultPos = panelRect.anchoredPosition;
+        PanelCloseImmediate();
     }
 
+    /// <summary>
+    /// パネルを開く(アニメーション)
+    /// </summary>
     public void PanelOpen()
     {
-        menuButtons.SetActive(false);
         asobikata.SetActive(true);
         modeExplain.SetActive(false);
         howToPlayPanel.SetActive(true);
+
+        // 初期状態
+        panelRect.anchoredPosition = defaultPos + Vector2.up * moveY;
+        canvasGroup.alpha = 0f;
+
+        // アニメーション
+        panelRect.DOAnchorPos(defaultPos, animDuration).SetEase(Ease.OutCubic);
+        canvasGroup.DOFade(1f, animDuration);
     }
 
+    /// <summary>
+    /// パネルを閉じる(アニメーション)
+    /// </summary>
     public void PanelClose()
     {
-        menuButtons.SetActive(true);
+        canvasGroup.DOFade(0f, animDuration);
+        panelRect
+            .DOAnchorPos(defaultPos + Vector2.up * moveY, animDuration)
+            .SetEase(Ease.InCubic)
+            .OnComplete(() =>
+            {
+                howToPlayPanel.SetActive(false);
+            });
+    }
+
+    /// <summary>
+    /// パネルを素早く閉じる
+    /// </summary>
+    private void PanelCloseImmediate()
+    {
         howToPlayPanel.SetActive(false);
+        canvasGroup.alpha = 0f;
+        panelRect.anchoredPosition = defaultPos;
     }
 
     /// <summary>
